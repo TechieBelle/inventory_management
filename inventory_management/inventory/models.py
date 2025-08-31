616fdae7bab07b864652de847738f865d6888b69
Folashade Bello <folashadebello.b@gmail.com>
2025-08-31 20:23:48 +0100

    Configured settings for DRF, JWT auth, and custom user model

--- inventory_management/inventory/models.py
+++ inventory_management/inventory/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class InventoryItem(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="items"
+    )
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=50, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.quantity})"
+
+
+class ChangeLog(models.Model):
+    item = models.ForeignKey(InventoryItem, on_delete=models.CASCADE, related_name="logs")
+    old_quantity = models.IntegerField()
+    new_quantity = models.IntegerField()
+    changed_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    changed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.item.name} changed by {self.changed_by.email}"
--- inventory_management/inventory_management/settings.py
+++ inventory_management/inventory_management/settings.py
@@ -27,9 +27,21 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #local apps
     'inventory',  
+    'accounts',
+    #third party apps
     'rest_framework',
+    'rest_framework_simplejwt',
+    'drf_spectacular',
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -110,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = "accounts.CustomUser"
